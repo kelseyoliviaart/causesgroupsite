07851c94ee715e4e7009994d46f3977b8ed29b64
Himanshu Anand <him.anand.19@gmail.com>
2015-02-12 10:22:07 -0500

    Added javascript to incude header in all pages, updated index.html and gallery.html

---
+++ js/include.js
@@ -0,0 +1,17 @@
+// $(function(){
+//       $("#includedContent").load("header.html"); //does not work with file:// 
+//     });
+document.write('\
+	\
+			<div class="topbar">\
+			<img src="images/asha_logo.png" id="logo">\
+			<nav id="navbar">\
+				<ul class="nav">\
+					<li><a href="index.html">Home</a></li>\
+					<li><a href="gallery.html">Gallery</a></li>\
+					<li><a href="about.html">About Us</a></li>\
+					<li><a href="mailto:contactus@asha.org">Contact Us</a></li>\
+				</ul>\
+			</nav>\
+		</div>\
+	');
