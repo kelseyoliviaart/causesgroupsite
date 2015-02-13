b81294faea1fba804dec613f0a79b913682301b4
Himanshu Anand <him.anand.19@gmail.com>
2015-02-12 20:25:47 -0500

    added files to use bootstrap

---
+++ js/include_with_bootsrap.js
@@ -0,0 +1,23 @@
+// $(function(){
+//       $("#includedContent").load("header.html"); //does not work with file:// 
+//     });
+document.write('\
+	\
+			<div class="topbar">\
+			     <div class="container">\
+      				<nav class="navbar navbar-inverse navbar-fixed-top">\
+        				<div class="navbar-header">\
+          					<a class="navbar-brand" href="#">Home</a>\
+							<a class="navbar-brand" href="gallery.html">Gallery</a>\
+							<a class="navbar-brand" href="about.html">About Us</a>\
+							<a class="navbar-brand" href="mailto:contactus@asha.org">Contact Us</a>\
+        				</div>\
+        			</nav>\
+        		</div>\
+        		<div class="container">\
+        			<div class="row">\
+						<img src="images/asha_logo.png" >\
+					</div>\
+				</div>\
+			</div>\
+	');
