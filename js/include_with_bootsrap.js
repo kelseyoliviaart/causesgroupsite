ed9ef0d000970a1710bdd47938f74a00b107e2ff
Himanshu Anand <him.anand.19@gmail.com>
2015-02-16 01:05:38 -0500

    site updated with bootstrap

--- js/include_with_bootsrap.js
+++ js/include_with_bootsrap.js
@@ -4,20 +4,31 @@
 document.write('\
 	\
 			<div class="topbar">\
-			     <div class="container">\
-      				<nav class="navbar navbar-inverse navbar-fixed-top">\
-        				<div class="navbar-header">\
-          					<a class="navbar-brand" href="#">Home</a>\
+				<div class="container">\
+        				<img src="images/asha_logo.png">\
+     				<nav class="navbar navbar-inverse">\
+ 	      				<div class="navbar-header">\
+          					<a class="navbar-brand" href="index.html">Home</a>\
 							<a class="navbar-brand" href="gallery.html">Gallery</a>\
+							<a class="navbar-brand" href="donation.html">Donate</a>\
 							<a class="navbar-brand" href="about.html">About Us</a>\
 							<a class="navbar-brand" href="mailto:contactus@asha.org">Contact Us</a>\
         				</div>\
         			</nav>\
-        		</div>\
-        		<div class="container">\
-        			<div class="row">\
-						<img src="images/asha_logo.png" >\
-					</div>\
-				</div>\
 			</div>\
+        		</div>\
 	');
+
+$(document).ready(function(){
+           $('li img').on('click',function(){
+                var src = $(this).attr('src');
+                var img = '<img src="' + src + '" class="img-responsive"/>';
+                $('#myModal').modal();
+                $('#myModal').on('shown.bs.modal', function(){
+                    $('#myModal .modal-body').html(img);
+                });
+                $('#myModal').on('hidden.bs.modal', function(){
+                    $('#myModal .modal-body').html('');
+                });
+           });  
+        });
